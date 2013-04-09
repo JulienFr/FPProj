09241dd2733786814cad05fd4a9dab780f5b3f4f
Julien <jmalvot@gmail.com>
2013-04-09 11:56:10 +0200

    Unification Implementation, inline GCD operator, BetaReduction with Expression Value  in Interpreter

--- Main.hs
+++ Main.hs
@@ -1,90 +1,171 @@
-{-# OPTIONS_GHC -w #-}
------------------------------------------------------------------------------
---
--- Module      :  Main
--- Copyright   :
--- License     :  AllRightsReserved
---
--- Maintainer  :
--- Stability   :
--- Portability :
---
--- |
---
------------------------------------------------------------------------------
-
 module Main where
 
 import Parser
+import qualified Data.IntMap as M
 
+-- Greater Common Divisor
+exGCD = "(fix gcd . \\t . \\a . \\b . ifzero b a (gcd t (minus a (mul b (div a b))) b))";
+{- Euclide Algorithm used for Greatest Common Divisor
+function gcd(a, b)
+    while b ≠ 0
+       t := b
+       b := a mod t
+       a := t
+    return a		
+-}
+exGCD1 = exGCD ++ " 0 25 10";
+
+example0 = "minus 5 2"
+example01 = "ifzero 1 5 2"
+example02 = "plus 5 (minus 5 3)"
 example1 = "(fix fact . \\x . ifzero x 1 (mul x (fact (minus x 1)))) 10"
 example2 = "(\\x . mul x (minus x 1)) 10"                                     -- (10*(10-1))                = VI 90
 example3 = "(\\x . plus x (plus x 1)) 5"                                      -- (5+(5+1))                  = VI 11
 example4 = "(let w = (\\x . ifzero x 5 (minus x 1)) 5 in (\\y . plus y w) 4)" -- (ifz 5 5 (5-1)) into (4+x) = VI 8
-example5 = "gcd 125 (gcd 10 25)"                                              -- (GCD (125;GCD(10;25))      = VI 5
+--example5 = "gcd 125 (gcd 10 25)"                                              -- (GCD (125;GCD(10;25))      = VI 5
+example5 = "let x = (minus 4 1) in x"
 example6 = "mul (minus 5 2) (div 125 (div 25 10))"                            -- (5-2)*(125/(25/10))        = VI 186
-
-{- Euclide Algorithm used for Greatest Common Divisor
-    function gcd(a, b)
-        if a = 0
-           return b
-        while b /= 0
-            if a > b
-               a := a âˆ’ b
-            else
-               b := b âˆ’ a
-        return a
--}
+example7 = "let x = plus (let y = 5 in plus y 1) 5 in mul x 2" 
+example8 = "(\\x . \\y . plus x y) 5 6"
+example9 = "(\\x . \\y . \\t . plus x (minus t y)) 5 6 10"
+example10 = "(\\x . plus 4 x) ((\\y . minus y 5) 10)"
 
 -- Definition of an environment with pairs of (Variable, Value) and Value being an integer or a function of integers
-data Typ = N | !Typ :> !Typ deriving (Show, Eq)
+data Typ = TInt | !Typ :> !Typ | TV TVarName deriving (Show, Eq)
+type TVarName = Int
+-- variable environment
+data TVE = TVE Int (M.IntMap Typ)
 type TEnv = [(Var, Typ)]
 
 infixl 9 :>
 
--- Working environment with empty list of (Variable, Type) pairs
-tenv0 :: TEnv
-tenv0 = [
-        ("ifzero", N :> (N :> (N :> N))),
-        ("plus", N :> (N :> N)),
-        ("minus", N :> (N :> N)),
-        ("div", N :> (N :> N)),
-        ("mul", N :> (N :> N)),
-        ("gcd", N :> (N :> N))
-       ]
+-- Working Variable Environment with empty context initialized afterwards
+tve0 :: TVE
+tve0 = TVE 0 M.empty
 
--- Look up the variable to check if existing in the environment
-tlkup :: TEnv -> Var -> Typ
-tlkup tenv x = maybe err id $ lookup x tenv
-        where err = error $ "Unbound variable " ++ x
-        
--- Append (variable, value) in the environment
+tenv0 :: TEnv     -- Initialize context of the variable environment
+tenv0 = [ ("ifzero", TInt :> (TInt :> (TInt :> TInt))),
+		  ("plus", TInt :> (TInt :> TInt)),
+		  ("minus", TInt :> (TInt :> TInt)),
+		  ("div", TInt :> (TInt :> TInt)),
+		  ("mul", TInt :> (TInt :> TInt)) 
+	]
+
+tlkup :: TEnv -> Var -> Typ	
+tlkup env x = maybe err id $ lookup x env
+	where err = error $ "Unbound variable " ++ x
+	
 text :: TEnv -> (Var,Typ) -> TEnv
-text tenv xt = xt : tenv
+text env xt = xt : env
 
---tsel :: Exp -> Maybe Exp
---tsel tenv exp = 
+-- Look up the equation to check if existing in the environment
+tvlkup :: TVE -> TVarName -> Maybe Typ
+tvlkup (TVE _ s) v = M.lookup v s
+        
+-- Append typed equation in the environment
+tvext :: TVE -> (TVarName,Typ) -> TVE
+tvext (TVE c s) (tv,t) = TVE c (M.insert tv t s)
 
--- Type Checker
+-- Insert a new equation
+newtv :: TVE -> (Typ,TVE)
+newtv (TVE n s) = (TV n, TVE (succ n) s)
+
+-- Chase equations
+tvsub :: TVE -> Typ -> Typ
+tvsub tve (t1 :> t2) = tvsub tve t1 :> tvsub tve t2
+tvsub tve (TV v) | Just t <- tvlkup tve v = tvsub tve t
+tvsub tve t = t
+
+-- Unification algorithm
+unify :: Typ -> Typ -> TVE -> Either String TVE
+unify t1 t2 tve = unify2 (tvchase tve t1) (tvchase tve t2) tve -- chase t1 t2 into unify'
+
+-- shallow chase through a substitution :
+-- stop at the last equivalent type variable
+tvchase :: TVE -> Typ -> Typ
+tvchase tve (TV v) | Just t <- tvlkup tve v = tvchase tve t
+tvchase _ t = t
+
+-- If either t1 or t2 are type variables, they must be unbound
+unify2 :: Typ -> Typ -> TVE -> Either String TVE
+unify2 TInt TInt = Right                 -- two constants unify
+unify2 (TV v1) t2 = unifyv v1 t2         -- unify' left variable with right term
+unify2 t1 (TV v2) = unifyv v2 t1         -- unify' left term with right variable
+unify2 (t1a :> t1r) (t2a :> t2r) = either Left (unify t1r t2r) . unify t1a t2a -- unify' left terms and unify' right terms
+unify2 t1 t2 = const (Left $ unwords ["constant mismatch:", show t1, "and", show t2]) -- unknown case
+
+-- Unify a free variable v1 with v2
+unifyv :: TVarName -> Typ -> TVE -> Either String TVE
+unifyv v1 (TV v2) tve =
+	if v1 == v2 
+		then Right tve
+	else Right (tvext tve (v1, TV v2)) -- record new constraint
+unifyv v1 t2 tve = 
+	if occurs v1 t2 tve 
+		then Left $ unwords ["occurs check:", show (TV v1), "in", show (tvsub tve t2)]   -- case of occur-check ("x=f(x)") 
+	else Right (tvext tve (v1,t2))
+	
+-- The occurs check: if v appears free in t
+occurs :: TVarName -> Typ -> TVE -> Bool
+occurs v TInt _ = False    -- if v is a constant
+occurs v (t1 :> t2) tve = occurs v t1 tve || occurs v t2 tve -- in case of a typed function (t1 :> t2)
+occurs v (TV v2) tve =     -- in case of a type variable
+	case tvlkup tve v2 of
+		Nothing -> v == v2
+		Just t -> occurs v t tve
+
+-- Type Checker Helper (does almost all of the type-checker's job
+teval2 :: TEnv -> Exp -> (TVE -> (Typ,TVE))         
+teval2 env (BuiltIn (Nat n)) = \tve0 -> (TInt,tve0)           -- integer constant
+teval2 env (Abstract x e) = \tve0 ->
+	let
+	(tv,tve1) = newtv tve0
+	(te,tve2) = teval2 (text env (x, tv)) e tve1
+	in (tv :> te, tve2)
+teval2 env (Fix x t) = \tve0 -> teval2 env ((Abstract x t) :$ (Fix x t)) tve0 -- fix x.M
+teval2 env (Let x e1 e2) = \tve0 ->                             -- let
+	let
+	(t1, tve1) = teval2 env e1 tve0
+	in teval2 (text env (x, t1)) e2 tve1
+teval2 env (LVar "ifzero" :$ e1 :$ e2 :$ e3) = \tve0 ->   -- ifzero primitive function
+	let
+	(t1,tve1) = teval2 env e1 tve0
+	(t2,tve2) = teval2 env e2 tve1
+	(t3,tve3) = teval2 env e3 tve2
+	in case unify t1 TInt tve3 of -- if the first arg unifies with TInt
+		Right tve -> case unify t2 t3 tve of -- both last args need unify
+			Right tve -> (t2,tve)
+			Left err -> error $ unwords ["Branches of ifzero have different types\nUnification failed:", err]
+		Left err -> error $ "Trying to compare a non-integer to 0: " ++ err
+teval2 env (LVar x :$ e1 :$ e2) = \tve0 ->                      -- primitive functions in initial context
+	let 
+	v = tlkup env x -- primitive variable
+	(t1,tve1) = teval2 env e1 tve0 -- first arg
+	(t2,tve2) = teval2 env e2 tve1 -- second arg
+    in case either Left (unify t2 TInt) . unify t1 TInt $ tve2 of -- if both args unify with TInt
+	    Right tve -> (TInt,tve)	-- if they do, return TInt
+	    Left err -> error $ "Trying to operate non-integers: " ++ err
+teval2 env (LVar x) = \tve0 -> (tlkup env x, tve0)
+teval2 env (e1 :$ e2) = \tve0 ->                            -- lambda application
+	let
+	(t1,tve1) = teval2 env e1 tve0
+	(t2,tve2) = teval2 env  e2 tve1 
+	(t1r,tve3)= newtv tve2
+	in case unify t1 (t2 :> t1r) tve3 of
+		Right tve -> (t1r,tve)
+		Left err -> error err
+
+-- Type-Checker (chase type vars as far as possible)
 teval :: TEnv -> Exp -> Typ
-teval tenv (BuiltIn (Nat n)) = N                                    -- Natural Integer
-teval tenv (Fix x t)         = teval (text tenv (x,N :> N)) t       -- fix x.M
-teval tenv (Let x t1 t2)     = teval (text tenv (x, teval tenv t1)) t2   -- let ... in ...
-teval tenv (Abstract x t)    = (N :> teval (text tenv (x, N)) t)    -- Lambda abstraction
-teval tenv (LVar x)          = tlkup tenv x                         -- Variable
-teval tenv (t1 :$ t2)        =                                      -- Function application
-    let v1 = teval tenv t1
-        v2 = teval tenv t2
-    in case v1 of
-       v1a :> v1r | v1a == v2 -> v1r
-       v1a :> v1r -> error $ unwords ["Applying a function of arg type",
-                                      show v1a, "to argument of type",
-                                      show v2]
-       v1 -> error $ "Trying to apply a non-function: " ++ show v1
+teval tenv e = 
+	let (t,tve) = teval2 tenv e tve0 
+		in tvsub tve t
+		
 ----------------------------------------------------------------------------------------
-data Value = VI Integer
-            | VC (Value -> Value)
-            | VT (Value -> Value -> Value)
+data Value = VI Integer                    -- Integer Value
+			| VE Exp                       -- Expression Value
+            | VC (Value -> Value)          -- Function from one Value
+            | VT (Value -> Value -> Value) -- Function from two Values
 
 type Env = [(Var, Value)]
 
@@ -93,8 +174,7 @@ env0 = [
         ("plus", VT vtplus),
         ("minus", VT vtminus),
         ("mul", VT vtmul),
-        ("div", VT vtdiv),
-        ("gcd", VT vtgcd)
+        ("div", VT vtdiv)
         ]
 
 vtplus :: Value -> Value -> Value                                    -- primitive function plus
@@ -117,20 +197,6 @@ vtdiv e1 e2 =
     case (e1,e2) of
         (VI n1, VI n2) -> VI (n1 `div` n2)
         vs -> error $ "Trying to divide non-integers: " ++ show vs
-vtgcd :: Value -> Value -> Value                                     -- euclid's algorithm - greatest common divisor
-vtgcd e1 e2 =
-    case (e1,e2) of
-        (VI n1, VI n2) -> if (n1 == 0)
-                            then VI n2
-                          else VI (gcd2 n1 n2)
-        vs -> error $ "Trying to get the greatest common divisor from non-integers: " ++ show vs
-gcd2 :: Integer -> Integer -> Integer
-gcd2 a b =
-    case b of
-    0 -> a
-    _ -> case (a > b) of
-        True  -> gcd2 (a-b) b
-        False -> gcd2 a (b-a)
 
 ifzero :: Env -> Exp -> Exp -> Exp -> Value                          -- primitive function ifzero
 ifzero env e1 e2 e3 =
@@ -143,51 +209,51 @@ ifzero env e1 e2 e3 =
 -- Look up the variable to check if existing
 lkup :: Env -> Var -> Value
 lkup env x = maybe err id $ lookup x env
-  where err = error $ "Unbound variable " ++ x
-{-
-let y = lookup x tenv
-        in case y of
-        Nothing -> Undefined
-        Just Undefined ->
-        Just z -> z-}
+  where err = error $ "Unbound variable or term " ++ x
+
 -- Append (variable, value) in the environment
 ext :: Env -> (Var,Value) -> Env
 ext env xt = xt : env
 
 instance Show Value where
     show (VI n) = "VI " ++ show n
+    show (VE _) = "<term value>"
     show (VC _) = "<coupled function>"
     show (VT _) = "<tripled function>"
 
 -- Simple Interpreter
 eval :: Env -> Exp -> Value
-eval env (BuiltIn (Nat n)) = VI n                                     -- Natural Integer
-eval env (Fix x t)         = eval env (Abstract x (Fix x t))          -- fix x.M
-eval env (Let x e1 e2)     = eval (ext env (x, eval env e1)) e2       -- let ... in ...
-eval env (Abstract x e)    = VC (\v -> eval (ext env (x,v)) e)        -- Lambda abstraction
-eval env (LVar x)          = lkup env x                               -- Variable
+eval env (BuiltIn (Nat n))   = VI n                                     -- Natural Integer
+eval env (Fix x t)           = eval env ((Abstract x t) :$ (Fix x t))   -- fix x.M
+eval env ((Abstract x e):$ t)= eval (ext env (x, VE t)) e               -- Beta Reduction (\x.e) t = e[x:=t]
+eval env (Let x e1 e2)       = eval (ext env (x, eval env e1)) e2       -- let ... in ...
+eval env (Abstract x e)      = VC (\v -> eval (ext env (x,v)) e)        -- Lambda abstraction
+eval env (LVar x)            = 										    -- Variable
+		let y = lkup env x 
+		in case y of
+		VE e -> eval env e 
+		_ -> y
 eval env (LVar "ifzero" :$ e1 :$ e2 :$ e3) = ifzero env e1 e2 e3
-eval env (e1 :$ e2 :$ e3)  =                                          -- Tripled Function application
-    let v1 = eval env e1
-        v2 = eval env e2
-        v3 = eval env e3
-    in case v1 of
-        VT f -> f v2 v3
-        v -> error $ "Trying to apply a non-tripled-function: " ++ show v
-eval env (e1 :$ e2)        =                                          -- Coupled Function application
+eval env (LVar x :$ e1 :$ e2)    =                                    -- Primitive Function application
+    let v0 = lkup env x
+	in case v0 of
+		VT ft -> ft (eval env e1) (eval env e2)
+		_ -> v0
+eval env (e1 :$ e2) =                                                 -- Coupled Function application
     let v1 = eval env e1
         v2 = eval env e2
     in case v1 of
         VC f -> f v2
+        VE e -> eval env (e :$ e2)  -- terms from beta reduction interpreted
         v -> error $ "Trying to apply a non-coupled-function: " ++ show v
-
+		
 -- Main function
 main = do
         str <- getLine
         print str
         let exp = (lambda . lexer) str
         let typ = teval tenv0 exp
-        if (typ == N)
+        if (typ == TInt)
             then do print "Typed-checking successful\nInterpretation in process\n"
                     let res = eval env0 exp
                     print res
